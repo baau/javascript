32986a19c8888396b5d78e6fefc5082ac1d98ace
soc.bharat <soc.bharat@gmail.com>
2018-11-09 13:41:37 +0530

    Initialize Repository

---
+++ index.js
@@ -0,0 +1,50 @@
+//Adds Reverse to all String types
+String.prototype.reverse = function(){
+  return Array.from(this).reverse().join("");
+}
+
+// // Recerse a string
+//
+// function reverse(stringToReverse){
+//
+//   return Array.from(stringToReverse).reverse().join("");
+// }
+
+// Returns true for palindrome, false otherwise.
+function palindrome(string){
+  let processedContent = string.toLowerCase();
+  return processedContent === processedContent.reverse();
+}
+
+// Defining the Phrase Object
+function Phrase(content){
+  this.content = content;
+
+  //returns processed content for palindrome checking
+  this.processedContent = function (){
+    return  this.content.toLowerCase();
+  }
+  // checks for palindrome content
+  this.palindrome = function (){
+
+    return this.processedContent() === this.processedContent().reverse();
+  };
+
+  // // Makes the phrase LOUDER.
+  // this.louder = function(){
+  //   return this.content.toUpperCase();
+  // };
+}
+
+// Defining Trenalated Phrase Object
+function TranslatedPhrase(content, translation){
+  this.content = content;
+  this.translation = translation;
+
+  //returns trnslation processed for palindrome testing
+  this.processedContent = function(){
+    return this.translation.toLowerCase();
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
