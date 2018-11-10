91c839e60b620244a8a43f2d4fd9371f5457156f
soc.bharat <soc.bharat@gmail.com>
2018-11-10 14:53:45 +0530

    Finishing working on Palindrome Method

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+module.exports = Phrase;
+
 //Adds Reverse to all String types
 String.prototype.reverse = function(){
   return Array.from(this).reverse().join("");
@@ -11,18 +13,23 @@ String.prototype.reverse = function(){
 // }
 
 // Returns true for palindrome, false otherwise.
-function palindrome(string){
-  let processedContent = string.toLowerCase();
-  return processedContent === processedContent.reverse();
-}
+// function palindrome(string){
+//   let processedContent = string.toLowerCase();
+//   return processedContent === processedContent.reverse();
+// }
 
 // Defining the Phrase Object
 function Phrase(content){
   this.content = content;
 
+ // to provide only the alphabets in the string
+ this.letters = function(){
+    let regex = /[A-Za-z]/g;
+    return (this.content.match(regex).join("") || []);
+ }
   //returns processed content for palindrome checking
   this.processedContent = function (){
-    return  this.content.toLowerCase();
+    return  this.letters().toLowerCase();
   }
   // checks for palindrome content
   this.palindrome = function (){
@@ -37,14 +44,14 @@ function Phrase(content){
 }
 
 // Defining Trenalated Phrase Object
-function TranslatedPhrase(content, translation){
-  this.content = content;
-  this.translation = translation;
-
-  //returns trnslation processed for palindrome testing
-  this.processedContent = function(){
-    return this.translation.toLowerCase();
-  }
-}
-
-TranslatedPhrase.prototype = new Phrase();
+// function TranslatedPhrase(content, translation){
+//   this.content = content;
+//   this.translation = translation;
+//
+//   //returns trnslation processed for palindrome testing
+//   this.processedContent = function(){
+//     return this.translation.toLowerCase();
+//   }
+// }
+//
+// TranslatedPhrase.prototype = new Phrase();
